c748ef579b038e4e2bb9afcc83f5feda6ff9fad5
Ahmed Moussa <ahmmoussa22@gmail.com>
2021-01-30 16:20:03 -0500

    Boilerplate Modal Code

---
+++ boilerplate/modal.jsx
@@ -0,0 +1,15 @@
+<div className="modal-wrapper">
+    <div className="modal">
+        <div className="modal-header">
+            Modal Title
+            
+        </div>
+        <div className="modal-content">
+
+        </div>
+        <div className="modal-footer">
+            <button id="cancel" className="cancelButton" onClick={() => console.log('Cancel')}>Cancel</button>
+            <button id="submit" className="saveButton" onClick={() => console.log('Save')}>Save</button>
+        </div>
+    </div>
+</div>
