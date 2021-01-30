ca8c02e6f2056352d4c0b496abb0f8f5e45d0a76
bobbyngo <ngohuugiabao8980@gmail.com>
2021-01-30 17:41:45 -0500

    change parsing

---
+++ notification/changeparser.py
@@ -0,0 +1,30 @@
+import requests
+from time import time, sleep
+
+filename = 'sysc2004.html'
+
+#Course want to get: SYSC 2004
+#                     https://culearn.carleton.ca/moodle/course/view.php?id=167380 
+                    # 1qsk9usqtoj7r3ct00svsv1nmf
+
+#Get Method 
+r = requests.get("https://culearn.carleton.ca/moodle/course/view.php?id=167380")
+
+#Passing Parameter
+headers={'MoodleSession': '1qsk9usqtoj7r3ct00svsv1nmf'}
+r = requests.get('https://culearn.carleton.ca/moodle/course/view.php?id=167380', params=headers)
+
+#Looping 60s to check anything wrong
+running = True
+while running:
+    sleep (60 - time() % 60)
+
+    if ( filename!= headers["MoodleSession"] ):
+        running = False
+        print("Difference happens")
+
+
+#Response content
+print (r.text)
+
+print("Access successfully")
